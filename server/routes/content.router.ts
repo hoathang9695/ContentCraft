8befa4b2923d03e6f9785089bc0d1180d1c33353
thanghoa1420 <thanghoa1420@gmail.com>
2025-04-12 11:15:56 +0700

    fix(route)

--- server/routes/content.router.ts
+++ server/routes/content.router.ts
@@ -10,7 +10,7 @@ const contentController = new ContentController();
 router.get("/", isAuthenticated, contentController.getAllContents);
 router.get("/:id", isAuthenticated, contentController.getContentById);
 router.post("/", isAuthenticated, contentController.createContent);
-router.put("/:id", isAuthenticated, contentController.updateContent);
+router.patch("/:id", isAuthenticated, contentController.updateContent);
 router.delete("/:id", isAuthenticated, contentController.deleteContent);
 
 export default router;
